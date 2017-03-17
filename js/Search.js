f8fb97213bfca3a1df0dfe9392681e011fa709c3
dn0912 <nguyen.duc0912@googlemail.com>
2017-03-17 01:40:06 +0100

    get and set initial state

--- js/Search.js
+++ js/Search.js
@@ -3,12 +3,20 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
         <header>
-          <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
+          <h1>{this.state.searchTerm}</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
           {preload.shows.map((show) => {
