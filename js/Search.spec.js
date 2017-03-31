526d7b80fd12eaa1079847c7d3a3e41bec48adc8
dn0912 <nguyen.duc0912@googlemail.com>
2017-03-31 20:41:41 +0200

    testing reducers

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -25,6 +25,4 @@ test('Search should render correct amount of shows based on search', () => {
   const component = render(<Provider store={store}><Search shows={preload.shows} /></Provider>)
   const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
   expect(component.find('.show-card').length).toEqual(showCount)
-// const component = shallow(<Search />)
-  // component.find('input').simulate('change', {target:{value: searchWord}})
 })
