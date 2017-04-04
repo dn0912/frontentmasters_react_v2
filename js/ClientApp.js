c94924aefef52c461ee29df0b1f622fd2c443efb
dn0912 <nguyen.duc0912@googlemail.com>
2017-04-04 16:41:34 +0200

    fix

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router'
 import App from './App'
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementByID('app'))
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
