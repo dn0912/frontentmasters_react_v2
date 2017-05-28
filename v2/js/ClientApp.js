f1a280ca5580ab0615a173f02493588978b64708
dn0912 <nguyen.duc0912@googlemail.com>
2017-05-28 11:35:26 +0200

    lint

--- v2/js/ClientApp.js
+++ v2/js/ClientApp.js
@@ -1,9 +1,11 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
   render: function () {
-    return(
+    return (
       div(null,
         h1({style: {color: this.props.color, fontweight: 'bold'}}, this.props.title)
       )
